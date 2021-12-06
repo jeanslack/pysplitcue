059ac569b3abee89c33bfcacf6598109122b63ef
Gianluca Pernigotto <jeanlucperni@gmail.com>
2021-12-06 23:20:16 +0100

    fix missing quotes in the filenames

--- pysplc/splc.py
+++ pysplc/splc.py
@@ -1,158 +1,168 @@
-#
-# First release: 25/08/2012
-# 
-#########################################################
-# Name: pysplitcue
-# Porpose: wraps the shnsplit and cuetag commands
-# Platform: Mac OsX, Gnu/Linux
-# Writer: jeanslack <jeanlucperni@gmail.com>
-# license: GPL3
-# Rev: January 26 2015, Nov 21 2017, Nov 24 2017, Aug 8 2018
-#########################################################
+"""
+First release: 25/08/2012
+
+Name: pysplitcue
+Porpose: wraps the shnsplit and cuetag commands
+Platform: Mac OsX, Gnu/Linux
+Writer: jeanslack <jeanlucperni@gmail.com>
+license: GPL3
+Rev: January 26 2015, Nov 21 2017, Nov 24 2017, Aug 8 2018, Dec 06 2021
+Code checker: flake8 and pylint
+"""
 
 import sys
 import os
-from pysplc.str_utils import strings
 from shutil import which
 import argparse
 import subprocess
+from pysplc.str_utils import strings
 
-# check to essential
-if which('cuetag'):
-    cuetag = which('cuetag')
-elif which('cuetag.sh'):
-    cuetag = which('cuetag')
-else:
-    sys.stderr.write("pysplitcue:ERROR: cuetag is required, "
-                     "please install 'cuetools'.")
-    sys.exit(1)
-if not which('shntool'):
-    sys.stderr.write("pysplitcue:ERROR: 'shntool' is required, "
-                     "please install it.")
-    sys.exit(1)
-    
-# add useful information strings 
+# strings of information
 cr = strings()
-prg_name = cr[6]
-version = cr[3]
-release = cr[4]
-webpage = cr[7]
-blogspot = cr[8]
-long_help = cr[11]
-short_help = cr[12]
-try_help = cr[15]
+PRG_NAME = cr[6]
+VERSION = cr[3]
+RELEASE = cr[4]
+WEBPAGE = cr[7]
+BLOGSPOT = cr[8]
+LONG_HELP = cr[11]
+SHORT_HELP = cr[12]
+TRY_HELP = cr[15]
 
-#----------------------------------------------------------#
+
+def check_essentials():
+    """check the essentials """
+    if which('cuetag'):
+        cuetag = which('cuetag')
+    elif which('cuetag.sh'):
+        cuetag = which('cuetag')
+    else:
+        sys.stderr.write("pysplitcue:ERROR: cuetag is required, "
+                         "please install 'cuetools'.\n")
+        sys.exit(1)
+    if not which('shntool'):
+        sys.stderr.write("pysplitcue:ERROR: 'shntool' is required, "
+                         "please install it.\n")
+        sys.exit(1)
+
+    return cuetag
+# ----------------------------------------------------------#
+
+
 def dependencies():
     """
     Check for dependencies
-    
+
     """
-    listing = ['flac', 'mac', 'wavpack', 'shntool', 'cuebreakpoints', 
+    listing = ['flac', 'mac', 'wavpack', 'shntool', 'cuebreakpoints',
                'cueconvert', 'cueprint']
-    for required in  listing:
-        #if which(required):
+    for required in listing:
+        # if which(required):
         if which(required, mode=os.F_OK | os.X_OK, path=None):
-                
-            print ("Check for: '%s' ..Ok" % required)
+
+            print(f"Check for: '{required}' ..Ok")
         else:
-            print ("Check for: '%s' ..Not Installed" % required)
-    
+            print(f"Check for: '{required}' ..Not Installed")
+
     if which('cuetag', mode=os.F_OK | os.X_OK, path=None):
-        print ("Check for: 'cuetag' ..Ok")
-        
+        print("Check for: 'cuetag' ..Ok")
+
     elif which('cuetag.sh', mode=os.F_OK | os.X_OK, path=None):
-        print ("Check for: 'cuetag.sh' ..Ok")
+        print("Check for: 'cuetag.sh' ..Ok")
     else:
-        print ("Check for: 'cuetag' ..Not Installed")
-            
-#----------------------------------------------------------#
+        print("Check for: 'cuetag' ..Not Installed")
+# ----------------------------------------------------------#
+
+
 def run_process(in_ext, out_ext, name):
     """
     All final processes
-    
-    """
-    in_ext = in_ext.split('.')[1]
-    name = name.split('.')[0]
-    split_dict = {'wav:wav':
-            f'shnsplit -o wav -f {name}.cue -t "%n - %t.split" {name}.wav',
-                  'wav:flac':
-            f'shnsplit -o flac -f {name}.cue -t "%n - %t" {name}.wav',
-                  'wav:ape':
-            f'shnsplit -o ape -f {name}.cue -t "%n - %t" {name}.wav',
-                  'flac:wav':
-            f'shnsplit -o wav -f {name}.cue -t "%n - %t" {name}.flac',
-                  'flac:flac':
-            f'shnsplit -o flac -f {name}.cue -t "%n - %t" {name}.flac',
-                  'flac:ape':
-            f'shnsplit -o ape -f {name}.cue -t "%n - %t" {name}.flac',
-                  'ape:wav':
-            f'shnsplit -o wav -f {name}.cue -t "%n - %t" {name}.ape',
-                  'ape:flac':
-            f'shnsplit -o flac -f {name}.cue -t "%n - %t" {name}.ape',
-                  'ape:ape':
-            f'shnsplit -o ape -f {name}.cue -t "%n - %t.split" {name}.ape'
-                 }
 
-    tag_dict = {'wav:flac':f'{cuetag} {name}.cue *.flac', 
-                'flac:flac':f'{cuetag} {name}.cue *.flac', 
-                'ape:flac':f'{cuetag} {name}.cue *.flac'
+    """
+    cuetag = check_essentials()
+    in_ext = in_ext.replace('.', '', 1)
+    name = os.path.splitext(name)[0]
+
+    split_dict = {'wav:wav': (f'shnsplit -o wav -f "{name}.cue" '
+                              f'-t "%n - %t.split" "{name}.wav"'),
+                  'wav:flac': (f'shnsplit -o flac -f "{name}.cue" '
+                               f'-t "%n - %t" "{name}.wav"'),
+                  'wav:ape': (f'shnsplit -o ape -f "{name}.cue" '
+                              f'-t "%n - %t" "{name}.wav"'),
+                  'flac:wav': (f'shnsplit -o wav -f "{name}.cue" '
+                               f'-t "%n - %t" "{name}.flac"'),
+                  'flac:flac': (f'shnsplit -o flac -f "{name}.cue" '
+                                f'-t "%n - %t" "{name}.flac"'),
+                  'flac:ape': (f'shnsplit -o ape -f "{name}.cue" '
+                               f'-t "%n - %t" "{name}.flac"'),
+                  'ape:wav': (f'shnsplit -o wav -f "{name}.cue" '
+                              f'-t "%n - %t" "{name}.ape"'),
+                  'ape:flac': (f'shnsplit -o flac -f "{name}.cue" '
+                               f'-t "%n - %t" "{name}.ape"'),
+                  'ape:ape': (f'shnsplit -o ape -f "{name}.cue" '
+                              f'-t "%n - %t.split" "{name}.ape"')
+                  }
+
+    tag_dict = {'wav:flac': f'{cuetag} "{name}.cue" *.flac',
+                'flac:flac': f'{cuetag} "{name}.cue" *.flac',
+                'ape:flac': f'{cuetag} "{name}.cue" *.flac'
                 }
 
-    if '%s:%s' %(in_ext, out_ext) in split_dict.keys():
-        #print(split_dict[f'{in_ext}:{out_ext}'])#print command for debug
+    if f'{in_ext}:{out_ext}' in split_dict:
+        # print(split_dict[f'{in_ext}:{out_ext}'])#print command for debug
         try:
             command = split_dict[f'{in_ext}:{out_ext}']
-            subprocess.check_call(command, shell = True)
-            #makedir_move(".split.wav","Formato-wav")
+            subprocess.check_call(command, shell=True)
+            # makedir_move(".split.wav","Formato-wav")
             print("\033[1m...done.\033[0m")
-        
+
         except subprocess.CalledProcessError as err:
-            sys.exit("\033[31;1mProcess Error!\033[0m %s" % (err))
-        
-        if f'{in_ext}:{out_ext}' in tag_dict.keys():
+            sys.exit(f"\033[31;1mProcess Error!\033[0m {err}")
+
+        if f'{in_ext}:{out_ext}' in tag_dict:
             print("\nApply tags on audio tracks...\n")
             try:
                 command = tag_dict[f'{in_ext}:{out_ext}']
-                subprocess.check_call(command, shell = True)
-                #makedir_move(".split.wav","Formato-wav")
+                subprocess.check_call(command, shell=True)
+                # makedir_move(".split.wav","Formato-wav")
                 print("\033[1m...done.\033[0m")
-            
+
             except subprocess.CalledProcessError as err:
-                sys.exit("\033[31;1mProcess Error!\033[0m %s" % (err))
-                
-#----------------------------------------------------------#
+                sys.exit(f"\033[31;1mProcess Error!\033[0m {err}")
+# ----------------------------------------------------------#
+
+
 def checker(out_ext, filename):
     """
-    file check utility, evaluates if the file exists, 
-    if the audio format is supported and also evaluates 
+    file check utility, evaluates if the file exists,
+    if the audio format is supported and also evaluates
     whether the corresponding .cue file
     """
     filename = os.path.abspath(filename)
-    format_list = ['.wav','.flac','.ape']
+    format_list = ['.wav', '.flac', '.ape']
     cuelist = []
     name = os.path.basename(filename)
     in_ext = os.path.splitext(name)
-    
+
     if os.path.isfile(filename):
         os.chdir(os.path.dirname(filename))
-        for f in os.listdir():
-            if os.path.splitext(f)[1] in '.cue':
-                cuelist.append(f)
-        
+        for fname in os.listdir():
+            if os.path.splitext(fname)[1] in '.cue':
+                cuelist.append(fname)
+
         if not in_ext[1] in format_list:
-            sys.exit("pysplitcue: error: unrecognized input format '%s', "
-                     "choose between  %s" % (in_ext[1],format_list))
-                
-        if '%s.cue' % in_ext[0] in cuelist:
+            sys.exit(f"pysplitcue: error: unrecognized input format "
+                     f"'{in_ext[1]}', choose between  {format_list}")
+
+        if f'{in_ext[0]}.cue' in cuelist:
             run_process(in_ext[1], out_ext, name)
         else:
-            sys.exit("pysplitcue: error: No such CUE sheet file"
-                     "named: '%s.cue'" % in_ext[0])
+            sys.exit(f"pysplitcue: error: No such CUE sheet file"
+                     f"named: '{in_ext[0]}.cue'")
     else:
-        sys.exit("pysplitcue: error: No such file: '%s'" % (filename))
-        
-#----------------------------------------------------------#
+        sys.exit(f"pysplitcue: error: No such file: '{filename}'")
+# ----------------------------------------------------------#
+
+
 def main():
     """
     Parser of the users inputs (positional/optional arguments)
@@ -160,12 +170,12 @@ def main():
     parser = argparse.ArgumentParser(
                 description='Audio files cue splitting utility',)
     parser.add_argument(
-                '-v', '--version', 
+                '-v', '--version',
                 help="show the current version and exit",
                 action="store_true",
                        )
     parser.add_argument(
-                '-c', '--check', 
+                '-c', '--check',
                 help="list of installed or missing dependencies",
                 action="store_true",
                        )
@@ -177,17 +187,17 @@ def main():
     parser.add_argument(
                 '-i',
                 metavar='FILE',
-                #type=str, 
+                # type=str,
                 help="input audio filename to splitting",
-                #dest='enable_config',
-                #action='store_true',
-                #nargs='?',
-                #default=PWD,
-                #action="store_true"
+                # dest='enable_config',
+                # action='store_true',
+                # nargs='?',
+                # default=PWD,
+                # action="store_true"
                        )
-    
+
     args = parser.parse_args()
-    
+
     if args.check:
         dependencies()
     elif args.o:
@@ -195,13 +205,7 @@ def main():
             sys.exit('pysplitcue: error: missing option -i ..FILE')
         checker(args.o, args.i)
     elif args.version:
-        print('%s - %s' % (version, release))
+        print(f'{VERSION} - {RELEASE}')
         return
     else:
         print("Type 'pysplitcue -h' for help.")
-        
-#----------------------------------------------------------#
-if __name__ == "__main__":
-    main()
-
-    
--- pysplc/str_utils.py
+++ pysplc/str_utils.py
@@ -1,10 +1,9 @@
-#
-#########################################################
-# Name: str_utils
-# Porpose: used string
-# Writer: jeanslack <jeanlucperni@gmail.com>
-# license: GPL3
-#########################################################
+"""
+Name: str_utils
+Porpose: used string
+Writer: jeanslack <jeanlucperni@gmail.com>
+license: GPL3
+"""
 
 
 def strings():
@@ -14,16 +13,16 @@ def strings():
   author = "Gianluca Pernigotto - Jeanslack"
   mail = '<jeanlucperni@gmail.com>'
   copyright = '© 2013-2019'
-  version = '1.0.0'
-  release = 'August 8 2019'
+  version = '1.0.1'
+  release = 'December 06 2021'
   rls_name = "Pysplitcue"
   prg_name = "pysplitcue"
   webpage = "https://github.com/jeanslack/pysplitcue"
   blogspot = "http://itamburiditux.blogspot.it/search?q=pysplitcue"
   short_decript = 'A stupid wrapper interface for **Shntool** and **Cuetools** libraries.'
   long_desript = """
-Splitting utilities for big audio tracks with CUE sheet. 
-It support Wav, Flac and Ape audio formats and automatic tag. 
+Splitting utilities for big audio tracks with CUE sheet.
+It support Wav, Flac and Ape audio formats and automatic tag.
 Requires a  *.cue* sheet to read audio metadata to splitting and tagging.
 """
 
@@ -33,7 +32,7 @@ Blogspot: <%s>
     """ % (prg_name, version, webpage, blogspot)
 
   short_help = "Usage: pysplitcue [OPTION] ['PATH NAME']"
-  
+
   try_help = "Try: 'pysplitcue --help' for more information."
 
   license = """
@@ -56,7 +55,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 """ % (copyright, author, author, mail)
 
   short_license = "GPL3 (Gnu Public License)"
-  
-  return (author, mail, copyright, version, release, rls_name, prg_name, webpage, 
+
+  return (author, mail, copyright, version, release, rls_name, prg_name, webpage,
           blogspot, short_decript, long_desript, long_help, short_help, license,
           short_license, try_help)
